33455800ffedc1f9daa19a2a12ed302da3456512
ANAS727189 <144107389+ANAS727189@users.noreply.github.com>
2024-07-03 18:54:20 +0530

    Changed a project image

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import GithubIcon from '@mui/icons-material/GitHub';
 import "./footer.css";
 
 const Footer = () => {
@@ -19,7 +19,7 @@ const Footer = () => {
           <a href="#education" className="foot-link">Education</a>
         </nav>
         <div className="flex mt-4">
-          <a href="https://github.com/Anas727189" target="_blank" rel="noopener noreferrer" className="mx-4 text-xl text-gray-800 hover:text-[#BFA181] transition duration-300 ease-in-out transform hover:scale-110"><GitHubIcon /></a>
+          <a href="https://github.com/Anas727189" target="_blank" rel="noopener noreferrer" className="mx-4 text-xl text-gray-800 hover:text-[#BFA181] transition duration-300 ease-in-out transform hover:scale-110"><GithubIcon /></a>
           <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="mx-4 text-xl text-gray-800 hover:text-[#BFA181] transition duration-300 ease-in-out transform hover:scale-110"><TwitterIcon /></a>
           <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="mx-4 text-xl text-gray-800 hover:text-[#BFA181] transition duration-300 ease-in-out transform hover:scale-110"><LinkedInIcon /></a>
           <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="mx-4 text-xl text-gray-800 hover:text-[#BFA181] transition duration-300 ease-in-out transform hover:scale-110"><InstagramIcon /></a>
